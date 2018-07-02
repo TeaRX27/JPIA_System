3a522b1ed603f71efc0758575cdc71b9387a433d
LENOVO <LENOVO@ZERO>
2018-07-02 21:27:37 +0800

    >test cryptor engine

--- System/CryptorEngine.cs
+++ System/CryptorEngine.cs
@@ -52,30 +52,29 @@ namespace EnCryptDecrypt
         {
             byte[] keyArray;
             byte[] toEncryptArray = Convert.FromBase64String(cipherString);
-
+            byte[] resultArray;
             System.Configuration.AppSettingsReader settingsReader = new AppSettingsReader();
             //Get your key from config file to open the lock!
             string key = (string)settingsReader.GetValue("SecurityKey", typeof(String));
-            
-            if (useHashing)
-            {
-                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
-                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
-                hashmd5.Clear();
-            }
-            else
-                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+                if (useHashing)
+                {
+                    MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                    keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                    hashmd5.Clear();
+                }
+                else
+                    keyArray = UTF8Encoding.UTF8.GetBytes(key);
 
-            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
-            tdes.Key = keyArray;
-            tdes.Mode = CipherMode.ECB;
-            tdes.Padding = PaddingMode.PKCS7;
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
 
-            ICryptoTransform cTransform = tdes.CreateDecryptor();
-            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
-                        
-            tdes.Clear();
+                ICryptoTransform cTransform = tdes.CreateDecryptor();
+                resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                tdes.Clear();
             return UTF8Encoding.UTF8.GetString(resultArray);
+
         }
     }
 }
